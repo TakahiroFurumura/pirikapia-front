959186f0774549d19fbf3d80be5b2bc0f4e245e0
takahirofurumura <furimura@gmail.com>
2025-06-02 09:27:13 +0900

    Changes

---
+++ dist/spa/assets/use-timeout-DZvz3B1y.js
@@ -0,0 +1,1 @@
+import{j as u}from"./vm-Lib1ApYN.js";import{V as i,o as m,g as s}from"./index-C0m0Erjy.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};

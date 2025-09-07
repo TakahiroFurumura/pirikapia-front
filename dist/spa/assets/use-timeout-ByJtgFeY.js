dacfd432a4330d5c44d702b134baec6ceb9f0a0c
takahirofurumura <furimura@gmail.com>
2025-09-07 23:25:08 +0900

    Changes

---
+++ dist/spa/assets/use-timeout-ByJtgFeY.js
@@ -0,0 +1,1 @@
+import{v as u}from"./use-router-link-BNZ7x_Ga.js";import{Z as i,o as m,g as s}from"./index-B3FBFRuF.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
